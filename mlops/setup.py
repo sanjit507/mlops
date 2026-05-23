30ce941114a47cf8c7bcdba467cc269786b9b430
sanjit kumar <sanjitchaudhary506@gmail.com>
2026-05-23 11:56:07 +0530

    initial commit

---
+++ mlops/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project build for working understanding of mlops using small project',
+    author='sanjit',
+    license='',
+)
